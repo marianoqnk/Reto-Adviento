e6702cb9b39b10d1b7cb49105341aa0da250020b
marianoqnk <marianoqnk@gmail.com>
2021-12-11 21:43:16 +0100

    Reto10 fin

--- Reto10/main.cpp
+++ Reto10/main.cpp
@@ -3,9 +3,24 @@
 #include <cstdio>
 #include <iostream>
 #include <fstream>
+#include <stack>
+#include <functional>
+#include <algorithm>
+#include <vector>
 using namespace std;
 int counter = 0;
-int puntuacionTotal=0;
+long puntuacionTotal = 0;
+char pila[200];
+int ppila = 0;
+int incompletas=0;
+long long resultados[50];
+char cierre(char c)
+{
+    if (c == '(')
+        return '(' + 1;
+    else
+        return c + 2;
+}
 int leeDatos()
 {
     ifstream fe("input.txt");
@@ -19,77 +34,76 @@ int leeDatos()
         fe.getline(cadena, 1000, '\n'); // Load one line
         if (strlen(cadena) == 0)
             break;
-        // pch = strtok(cadena, "{([<>])}"); //open and closed Chunk
-        // char *in = new char[strlen(cadena)];
-        // strcpy(in, pch);
-        // all[counter++] = in;
         int points = 0;
-        for (auto c : cadena)
+        int y = 0;
+        for (y = 0; y < strlen(cadena); y++)
         {
-            switch (c)
+            char c = cadena[y];
+            // cout<<c;
+            if (c == '(' || c == '[' || c == '{' || c == '<')
             {
-            case '(':
-                parentesis++;
-                break;
-
-            case ')':
-                if (parentesis == 0) // line error
-                {
-                    error = true;
-                    points = 3;
-                    break;
-                }
-                parentesis--;
-                break;
-            case '[':
-                corchetes++;
-                break;
-
-            case ']':
-                if (corchetes == 0) // line error
-                {
-                    error = true;
-                    points = 57;
-                    break;
-                }
-                corchetes--;
-                break;
-            case '{':
-                llaves++;
-                break;
-
-            case '}':
-                if (llaves == 0) // line error
-                {
-                    error = true;
-                    points = 1197;
-                    break;
-                }
-                llaves--;
-                break;
-            case '<':
-                angulos++;
-                break;
-
-            case '>':
-                if (angulos == 0) // line error
-                {
-                    error = true;
-                    points = 25137;
-                    break;
-                }
-                angulos--;
-                break;
+                pila[++ppila] = c;
             }
-            if (error)
-                break;
+            else
+            {
+                char k = cierre(pila[ppila--]);
+                if (c != k)
+                {
+                    cout << "Error en linea,punto:" << counter << "," << y << " cara " << c << ' ' << k << '\n';
+                    switch (c)
+                    {
+                    case ')':
+                        points = 3;
+                        break;
+                    case ']':
+                        points = 57;
+                        break;
+                    case '}':
+                        points = 1197;
+                        break;
+                    case '>':
+                        points = 25137;
+                        break;
+                    }
+                    break;
+                };
+            }
         }
-        cout << "Puntos: " << points << '\n';
-        puntuacionTotal+=points;
+        if(points==0)//linea incompleta
+        {
+            //vaciar la pila
+            
+            long long points2=0;
+            while(ppila>0)
+            {
+                char c=cierre( pila[ppila--]);
+                cout<<c;
+                points2*=5;
+                switch (c)
+                    {
+                    case ')':
+                        points2 += 1;
+                        break;
+                    case ']':
+                        points2 += 2;
+                        break;
+                    case '}':
+                        points2 += 3;
+                        break;
+                    case '>':
+                        points2 += 4;
+                        break;
+                    }
+
+
+            }
+            cout<<points2<<'\n';
+            resultados[incompletas++]=points2;
+        }
+        puntuacionTotal += points;
+        ppila = 0;
+        counter++;
     } while (!fe.eof());
-    /*// Test data input
-    for (auto cadena : all)
-         cout << cadena << '\n';*/
     return counter;
 }
 
@@ -97,7 +111,14 @@ int main()
 {
     printf("Starting...\n");
     leeDatos();
-    cout<<"Total: "<<puntuacionTotal<<'\n';
+    cout << "Total: " << puntuacionTotal << '\n';
+    cout<<"Incompletas :"<<incompletas<<'\n';
+    std::vector<long long> mivector(resultados,resultados+incompletas);
+    std::sort(mivector.begin(),mivector.end());
+    int cuenta=1;
+    for (std::vector<long long>::iterator it=mivector.begin(); it!=mivector.end(); ++it)
+    std::cout<<cuenta++<< ' ' << *it<<'\n';
+        std::cout << '\n';
     return 0;
 }
-//155067
+// 155067
